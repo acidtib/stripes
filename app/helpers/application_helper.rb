9ae2ad7805371ebecf075d54d316e57b49f4b682
Dmitry Svetlichny <svetlichny@gmail.com>
2012-01-26 04:17:37 +0200

    simple regex username parser implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,11 @@ module ApplicationHelper
   def is_current? controller, action
     "current" if @current_controller == controller and @current_action == action
   end
+
+  def parse_usernames message
+    message.gsub /@([\w\d]*)/ do |match|
+      "<span class=\"user\">#{match}</span>"
+    end
+  end
   
 end
