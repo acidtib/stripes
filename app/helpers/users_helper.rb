cff441c6c76cb213dd69ef6d360d9a6e76378694
Dmitry Svetlichny <svetlichny@gmail.com>
2012-01-26 15:01:40 +0200

    added pluralizer to user counters in user profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+
+  def meta_pluralize number, noun, plural = nil
+    raw("<span>#{number || 0}</span>" + ((number == 1 || number =~ /%r^1(\.0+)?$/ ) ? noun : (plural || noun.pluralize)))
+  end
+
 end
