55403373120c147c5bf4abb643b88a3056c53573
Dmitry Svetlichny <svetlichny@gmail.com>
2012-01-26 11:10:47 +0200

    google maps image is now clickable

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,7 +1,7 @@
 module PhotosHelper
   
   def render_location_image x, y
-    raw("<img id=\"map\" src=\"http://maps.googleapis.com/maps/api/staticmap?center=#{x},#{y}&zoom=16&size=358x150&sensor=false&markers=color:red|#{x},#{y}\">")
+    raw("<a href=\"http://maps.google.com/maps?q=#{x},#{y}\" title=\"See the location on Google Maps\" target=\"_blank\"><img id=\"map\" src=\"http://maps.googleapis.com/maps/api/staticmap?center=#{x},#{y}&zoom=16&size=358x150&sensor=false&markers=color:red|#{x},#{y}\"></a>")
   end
     
 end
